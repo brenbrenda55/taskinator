9944734f6d518cfecffae925e2ad3db5b7ff2358
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-08-04 20:51:47 -0500

    trouble

---
+++ assests/script.js
@@ -0,0 +1,18 @@
+console.log(window.document);
+var btn = window.document.querySelector("button");
+console.dir(btn);
+document.querySelector("button").textContent;
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+  });
+
+  document.createElement("li");
+  var taskItemEl = document.createElement("li");
+  taskItemEl.textContent = "hello";
+taskItemEl;
+
+
+
+
