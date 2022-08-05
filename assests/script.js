7f5bbeabd8793cdcd5d145857c6e1149d755683a
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-08-04 21:39:16 -0500

    help

--- assests/script.js
+++ assests/script.js
@@ -12,6 +12,22 @@ buttonEl.addEventListener("click", function() {
   var taskItemEl = document.createElement("li");
   taskItemEl.textContent = "hello";
 taskItemEl;
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskItemEl = document.createElement("li");
+taskItemEl.textContent = "hello";
+tasksToDoEl.appendChild(taskItemEl);
+taskItemEl.className = "task-item";
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
 
 
 
